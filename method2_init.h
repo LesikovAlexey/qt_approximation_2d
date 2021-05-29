0a9debabb97455aaa395837b9c12268cfe21d00d
Alexey Lesikov <49493448+LesikovAlexey@users.noreply.github.com>
2021-05-30 00:09:25 +0300

    Delete method2_init.h

--- method2_init.h
+++
@@ -1,4 +0,0 @@
-#include <stdlib.h>
-int method2_init(int n_appr_x, double a_x, double b_x, int n_appr_y, double a_y, double b_y, double *values, double *derivatives, double **state);
-int method2_init_axis_x(int n_x, double a, double b, double *g);
-int method2_init_axis_y(int n_y, double a, double b, double *g);
